8c4b68934626eb322eded556043cd105913a328d
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-08-20 20:39:17 -0300

    adiciona exercício 1

---
+++ exercicio1.js
@@ -0,0 +1,40 @@
+/* exercício 1
+
+escrever três funções:
+
+ehPrimo(n: number) => boolean
+Receber um número e retornar um booleano 
+indicando se é primo ou não
+
+intervaloDePrimos(n1: number, n2: number) => number[]
+Receber dois números formando um intervalo e retornar 
+uma lista de primos no intervalo fechado [n1, n2]
+
+filtraPrimos(v: number[]) => number[]
+Receber uma lista de números e retornar uma nova lista
+contendo apenas os números primos da primeira lista
+
+*/
+
+function ehPrimo(n) {
+  // seu código aqui
+}
+
+console.log('true', ehPrimo(13))
+console.log('true', ehPrimo(17))
+console.log('true', ehPrimo(101))
+console.log('true', ehPrimo(379))
+console.log('false', ehPrimo(12))
+console.log('false', ehPrimo(39))
+console.log('false', ehPrimo(102))
+console.log('false', ehPrimo(381))
+
+function intervaloDePrimos(n1, n2) {
+  // seu código aqui
+}
+
+function filtraPrimos(v) {
+  // seu código aqui
+}
+
+
