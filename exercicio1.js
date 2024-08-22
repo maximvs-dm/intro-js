a6ae25d75662e2d402185b02ac8b3f2ae4f9403d
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-08-22 19:41:32 -0300

    solução da função ehPrimo

--- exercicio1.js
+++ exercicio1.js
@@ -17,17 +17,33 @@ contendo apenas os números primos da primeira lista
 */
 
 function ehPrimo(n) {
-  // seu código aqui
+  // é primo se for divisível apenas por 1 e por ele mesmo
+  if (n === 2) {
+    return true;
+  }
+
+  if (n % 2 === 0) {
+    return false;
+  }
+
+  for (let x = 3; x <= n ** 0.5 + 1; x += 2) {
+    if (n % x === 0) {
+      // x é divisor de n
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log('true', ehPrimo(13))
-console.log('true', ehPrimo(17))
-console.log('true', ehPrimo(101))
-console.log('true', ehPrimo(379))
-console.log('false', ehPrimo(12))
-console.log('false', ehPrimo(39))
-console.log('false', ehPrimo(102))
-console.log('false', ehPrimo(381))
+console.log("true", ehPrimo(13));
+console.log("true", ehPrimo(17));
+console.log("true", ehPrimo(101));
+console.log("true", ehPrimo(379));
+console.log("false", ehPrimo(12));
+console.log("false", ehPrimo(39));
+console.log("false", ehPrimo(102));
+console.log("false", ehPrimo(381));
 
 function intervaloDePrimos(n1, n2) {
   // seu código aqui
@@ -36,5 +52,3 @@ function intervaloDePrimos(n1, n2) {
 function filtraPrimos(v) {
   // seu código aqui
 }
-
-
