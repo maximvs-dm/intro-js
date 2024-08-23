d84764b6c653b228a5cb7d477c8dd8128ad923ae
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2024-08-22 22:37:25 -0300

    solução das outras 2 funções no exercício 1

--- exercicio1.js
+++ exercicio1.js
@@ -29,12 +29,15 @@ function ehPrimo(n) {
   for (let x = 3; x <= n ** 0.5 + 1; x += 2) {
     if (n % x === 0) {
       // x é divisor de n
+      // console.log(`${n} / ${x} = ${n / x}`);
       return false;
     }
   }
 
   return true;
 }
+
+// ehPrimo(360);
 
 console.log("true", ehPrimo(13));
 console.log("true", ehPrimo(17));
@@ -47,8 +50,26 @@ console.log("false", ehPrimo(381));
 
 function intervaloDePrimos(n1, n2) {
   // seu código aqui
+  const primos = [];
+
+  for (let x = n1; x <= n2; x++) {
+    if (ehPrimo(x)) {
+      primos.push(x);
+    }
+  }
+
+  return primos;
 }
 
+console.log("lista de 2 a 100");
+console.log(intervaloDePrimos(2, 100));
+console.log("lista de 400 a 500");
+console.log(intervaloDePrimos(400, 500));
+
 function filtraPrimos(v) {
-  // seu código aqui
+  return v.filter(ehPrimo);
 }
+
+lista = [3, 5, 8, 90, 45, 19, 27, 83, 101];
+console.log("filtra primos", lista);
+console.log(filtraPrimos(lista));
